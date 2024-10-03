f8e2092b635e8a0f7bbdd3a952269519231bce60
moetez1233 <moetezmaddouri@gmail.com>
2024-10-03 14:57:18 +0100

    add resilence4j to avoid exception if student service is down

--- school/src/main/java/com/micro/school/controller/SchoolController.java
+++ school/src/main/java/com/micro/school/controller/SchoolController.java
@@ -3,10 +3,12 @@ package com.micro.school.controller;
 import com.micro.school.entity.FullSchoolResponce;
 import com.micro.school.entity.School;
 import com.micro.school.service.SchoolImplement;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,6 +16,8 @@ import java.util.List;
 public class SchoolController {
     @Autowired
     private SchoolImplement schoolImplement;
+
+    public static final String SCHOOL_service="schoolService";
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -28,8 +32,12 @@ public class SchoolController {
     }
     @GetMapping("/with-students/{school-id}")
     @ResponseStatus(HttpStatus.OK)
-    public FullSchoolResponce getSchoolById(@PathVariable("school-id") Integer schoolId) {
+    @CircuitBreaker(name = SCHOOL_service,fallbackMethod = "getSchoolWhenException")
+    public Object getSchoolById(@PathVariable("school-id") Integer schoolId) {
        return schoolImplement.getSchoolWithStudents(schoolId);
 
     };
+    public String getSchoolWhenException(Integer schoolId,Throwable throwable) {
+        return "Student server is down ";
+    }
 }
